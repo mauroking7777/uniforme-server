e8cb5e254301a3310f15af44ca6ff803fa24ca7a
mauroking7777 <135770841+mauroking7777@users.noreply.github.com>
2025-09-18 12:35:07 -0400

    Update ordemItemArquivoRouter.js

--- routes/ordemItemArquivoRouter.js
+++ routes/ordemItemArquivoRouter.js
@@ -16,7 +16,6 @@ import {
   HeadObjectCommand,
 } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import fetch from 'node-fetch'; // se seu Node for >=18, pode remover e usar fetch global
 
 
 const router = express.Router();
